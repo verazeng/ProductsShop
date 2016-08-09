098e2ea9b2322fbe6c684ebdb4716114470acf88
Zeng Heng <zengheng810@gmail.com>
2016-08-09 16:25:04 +0800

    add data share options

--- ProductsShop/ViewController.swift
+++ ProductsShop/ViewController.swift
@@ -8,9 +8,29 @@
 
 import UIKit
 
+enum ShareDataOption : Int {
+    case AppGroup = 0, URLSchemeAndPasteboard, Airdrop
+    
+    static let allValues = [AppGroup, URLSchemeAndPasteboard, Airdrop]
+    
+    var title : String {
+        switch self {
+        case .AppGroup:
+            return "AppGroup"
+        case .URLSchemeAndPasteboard:
+            return "URLSchemeAndPasteboard"
+        case .Airdrop:
+            return "AirDrop"
+        }
+    }
+}
+
 class ViewController: UITableViewController {
 
     let products = ["Product 1", "Product 2", "Product 3", "Product 4", "Product 5", "Product 6", "Product 7", "Product 8"]
+    var dataShareOption : ShareDataOption = .AppGroup
+    
+    @IBOutlet weak var optionPicker: UIPickerView!
     
     @IBAction func orderProducts(_ sender: UIBarButtonItem) {
         if let indexes = tableView.indexPathsForSelectedRows {
@@ -21,10 +41,18 @@ class ViewController: UITableViewController {
         }
     }
     
-    func shareOrderedProducts(products: [String]) {
-        print("selected products: \(products)")
+    @IBAction func selectDataShareOptions(_ sender: UIBarButtonItem) {
+        optionPicker.isHidden = false
     }
     
+    func shareOrderedProducts(products: [String]) {
+        print("selected products: \(products)")
+        print("share option: \(dataShareOption.title)")
+    }
+    
+}
+
+extension ViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return products.count
     }
@@ -38,6 +66,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)!
         cell.accessoryType = .checkmark
+        optionPicker.isHidden = true
     }
     
     override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
@@ -46,3 +75,20 @@ class ViewController: UITableViewController {
     }
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ShareDataOption.allValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ShareDataOption(rawValue: row)!.title
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        dataShareOption = ShareDataOption(rawValue: row)!
+    }
+}
