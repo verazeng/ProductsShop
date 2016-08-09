ee156243bc1efe25c59a7e8208f34a1f8444f5f4
Zeng Heng <zengheng810@gmail.com>
2016-08-09 15:36:05 +0800

    Finish basic functions of order products

--- ProductsShop/ViewController.swift
+++ ProductsShop/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let products = ["Product 1", "Product 2", "Product 3", "Product 4", "Product 5", "Product 6", "Product 7", "Product 8"]
+    
+    @IBAction func orderProducts(_ sender: UIBarButtonItem) {
+        if let indexes = tableView.indexPathsForSelectedRows {
+            let selectedProducts = indexes.map({ (indexPath) -> String in
+                return products[indexPath.row]
+            })
+            shareOrderedProducts(products: selectedProducts)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func shareOrderedProducts(products: [String]) {
+        print("selected products: \(products)")
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView .dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        cell.textLabel?.text = products[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)!
+        cell.accessoryType = .checkmark
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)!
+        cell.accessoryType = .none
+    }
 }
 
